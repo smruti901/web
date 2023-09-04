dfef940dfaf8e6363662c3e089ad417a6ff81550
smruti901 <smrutimohapatra567@gmail.com>
2023-09-04 22:44:15 +0530

    Commit

---
+++ server.py
@@ -0,0 +1,77 @@
+from flask import Flask, redirect, render_template, request
+import csv
+app = Flask(__name__)
+print(__name__)
+app.debug = True
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+    with open('database.csv', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])     
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'something went wrong. Try again!'
+
+
+
+
+# @app.route('/contact.html')
+# def contact():
+#     return render_template('contact.html')
+
+# @app.route('/thankyou.html')
+# def thankyou():
+#     return render_template('thankyou.html')
+
+# @app.route('/work.html')
+# def work():
+#     return render_template('work.html')
+
+# @app.route('/works.html')
+# def works():
+#     return render_template('works.html')
+
+
+# @app.route('/about.html')
+# def about():
+#     return render_template('about.html')
+
+
+# @app.route('/blog')
+# def blog():
+#     return 'This is my Blog'
+
+# @app.route('/blog/2020')
+# def blog1():
+#     return 'This is my Bharati'
+app.run()
+
+
